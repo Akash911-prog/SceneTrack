91204f14c21e019095de218aa53ceed21e13aa30
Akash911-prog <akashsamanta0571@gmail.com>
2026-03-21 20:50:31 +0000

    chocolatey configs added

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'scenetrack' `
+  'https://github.com/Akash911-prog/scenetrack/releases/download/v1.0.5/scenetrack-windows-x64.zip' `
+  $toolsDir
